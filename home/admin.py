9b44eee3e33fb28fc6f37e7a89d7a35d6e9ce83c
vinicius-jesus <vjbfvinicius@hotmail.com>
2024-07-11 16:48:37 -0300

    Fix admin display for related modles

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import User, Product, Image
 # Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    exclude = ["status"]
+    inlines = [
+        ImageInline,
+    ]
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    exclude = ["description", "price", "status"]
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
